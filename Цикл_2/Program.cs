f6364ccd795bf74b6dddf73d8779712ed8fcaa96
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-05 00:05:13 +0200

    Добавьте файлы проекта.

---
+++ Цикл_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"Сумма всех чисел от 1 до 100: {sum}");
+    }
+}
